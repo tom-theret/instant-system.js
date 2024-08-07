7f9f234ac9a96240464f15387e1156e32ad30e58
Tom Theret <tomnils.theret@gmail.com>
2024-08-08 01:22:42 +0200

    types(v3): update disruption

--- src/model/v3/disruption.ts
+++ src/model/v3/disruption.ts
@@ -1,6 +1,14 @@
 import { DisruptionLevelV3 } from "./disruptionLevel";
 import { LineV3 } from "./line";
 
+export interface CurrentDisruptionV3 {
+  timestamp: string;
+  lines: lines[];
+}
+
+interface lines {
+  currentDisruptions: DisruptionV3[];
+}
 export interface DisruptionV3 {
   operatorId: string;
   id: string;
@@ -14,8 +22,7 @@ export interface DisruptionV3 {
   updateDate: string;
 }
 
-
-
 interface Message {
   text: string;
 }
+
